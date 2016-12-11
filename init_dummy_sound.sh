4b3149d3d991d98d4cfe3a0f96cb7204fc5d14e3
MolaynoxX <tobgle@rocketmail.com>
2016-12-11 18:26:26 +0100

    Trying to get dummy sound to work for tests with travis-ci

---
+++ init_dummy_sound.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cat << EOF > /home/travis/.asoundrc
+       pcm.dummy {
+          type hw
+          card 0
+       }
+
+       ctl.dummy {
+          type hw
+          card 0
+       }
+EOF
+chmod go+r /home/travis/.asoundrc
+cat << EOF >> /etc/modules.conf
+# OSS/Free portion - card #1
+alias sound-slot-0 snd-card-0
+alias sound-service-0-0 snd-mixer-oss
+alias sound-service-0-1 snd-seq-oss
+alias sound-service-0-3 snd-pcm-oss
+alias sound-service-0-8 snd-seq-oss
+alias sound-service-0-12 snd-pcm-oss
+EOF
+modprobe snd-dummy
+# ; modprobe snd-pcm-oss ; modprobe snd-mixer-oss ; modprobe snd-seq-oss
+# https://github.com/k3it/qsorder/blob/c16c6f3479ebd3bf34b3b41258f95974dc0ad24f/.travis.yml
